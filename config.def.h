259293714dc089b061dd4e6f5f381dc67ec11be6
Lucas Ramos <lucas.ramos@gft.com>
2021-10-08 19:05:32 -0300

    fix autostart and .desktop file

--- config.def.h
+++ config.def.h
@@ -1,21 +1,21 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx        = 2;        /* border pixel of windows */
-static const unsigned int snap            = 32;       /* snap pixel */
+static const unsigned int borderpx        = 2;   /* border pixel of windows */
+static const unsigned int snap            = 32;  /* snap pixel */
 static const unsigned int systraypinning  = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing  = 2;   /* systray spacing */
 static const int systraypinningfailfirst  = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray              = 1;        /* 0 means no systray */
-static const unsigned int gappih          = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv          = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh          = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov          = 10;       /* vert outer gap between windows and screen edge */
-static const int smartgaps                = 1;        /* 1 means no outer gap when there is only one window */
-static const int showbar                  = 1;        /* 0 means no bar */
-static const int topbar                   = 1;        /* 0 means bottom bar */
-static const char *fonts[]                = { "monospace:size=10" };
-static const char dmenufont[]             = "monospace:size=10";
+static const int showsystray              = 1;   /* 0 means no systray */
+static const unsigned int gappih          = 10;  /* horiz inner gap between windows */
+static const unsigned int gappiv          = 10;  /* vert inner gap between windows */
+static const unsigned int gappoh          = 10;  /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov          = 10;  /* vert outer gap between windows and screen edge */
+static const int smartgaps                = 1;   /* 1 means no outer gap when there is only one window */
+static const int showbar                  = 1;   /* 0 means no bar */
+static const int topbar                   = 1;   /* 0 means bottom bar */
+static const char *fonts[]                = { "Source Code Pro:size=10" };
+static const char dmenufont[]             = "Source Code Pro:size=10";
 static char normbgcolor[]                 = "#222222";
 static char normbordercolor[]             = "#444444";
 static char normfgcolor[]                 = "#bbbbbb";
@@ -29,7 +29,7 @@ static char *colors[][3] = {
 };
 
 static const char *const autostart[] = {
-	"st", NULL,
+	"sh", "-c", "~/.screenlayout/main.sh", NULL,
 	"slstatus", NULL,
 	"dunst", NULL,
 	"sxhkd", NULL,
@@ -75,13 +75,11 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "$TERMINAL", NULL };
-static const char *roficmd[] = { "rofi", "-show", "drun"};
+static const char *termcmd[]  = { "$TERMINAL", "st" };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
-	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
